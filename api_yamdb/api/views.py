81440892b01267a222e9df47001379d603327782
Alex <131307626+alxlen@users.noreply.github.com>
2024-03-07 23:02:53 +0300

    Update views.py
    recovery

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,27 @@
+from rest_framework import filters, viewsets
+
+from api.permissions import IsAdminUserOrReadOnly
+from api.serializers import (CategorySerializer, GenreSerializer,
+                             TitleSerializer)
+from reviews.models import Category, Genre, Title
+
+
+class CategoryViewSet():
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class GenreViewSet():
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
