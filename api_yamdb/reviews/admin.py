203a2594210c66645e6d631403c5290ecd96b98d
Dimacech <141561316+Dimacech@users.noreply.github.com>
2024-03-11 17:07:36 +0300

    d

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Comment, Genre, Review, Title, User
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
 @admin.register(Category)
