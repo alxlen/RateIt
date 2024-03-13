cda186ce621b12609021c8a19fe3c1d10fc1628e
Alex <lenko29@gmail.com>
2024-03-13 15:20:09 +0300

    isort

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,14 +1,13 @@
-from django.db.models import Avg
-from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets, views
+from rest_framework import filters, status, views, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import (LimitOffsetPagination)
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from rest_framework_simplejwt.tokens import AccessToken
 
 from api.filters import TitleFilter
